105e5ca12479a7ba7ccf7deee1793fa62a5aa2d0
Wesley Yep <wesley.yep@macquarie.com>
2018-06-01 20:28:20 +1000

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Authors</h2>
+    <ul>
+      <li v-for="a in authors" v-bind:key="a.id">
+        Name: {{a.name}},  ID: {{a.id}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'SPICE ALLEY!',
+      authors: [
+        {name: 'Tom', id: 'tomo'},
+        {name: 'Kayla', id: 'kaylo'},
+        {name: 'Wesley', id: 'wezzo'},
+        {name: 'Ryan', id: 'ryano'}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
